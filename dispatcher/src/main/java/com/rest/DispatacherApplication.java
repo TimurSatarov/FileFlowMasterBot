29a5427ba8894942840edd1d62149ba811629b58
Timur <timursatarov3@gmail.com>
2023-10-02 19:58:28 +0200

    DispacherAppliaction file(main file)

--- dispatcher/src/main/java/com/rest/DispatacherApplication.java
+++ dispatcher/src/main/java/com/rest/DispatacherApplication.java
@@ -4,6 +4,8 @@ package com.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//starter
 @SpringBootApplication
 public class DispatacherApplication {
    public static void main(String[] args) {
