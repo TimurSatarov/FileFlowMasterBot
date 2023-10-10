fb946e5ef05ef38c19b1d6eaaaf37111a390fdf9
TimurSatarov <timursatarov3@gmail.com>
2023-10-10 14:33:45 +0200

    init

--- dispatcher/src/main/java/com/rest/configuration/RabbitConfiguration.java
+++ dispatcher/src/main/java/com/rest/configuration/RabbitConfiguration.java
@@ -1,2 +1,14 @@
-package com.rest.configuration;public class RabbitConfiguration {
+package com.rest.configuration;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+
+    }
 }
